972634ad090e1ba873f557961871191477915d1d
Fendi <do1fendi@gmail.com>
2021-06-22 10:08:57 +0800

    added basePath

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <div className='text-center text-green-900 text-md font-bold'>
-            All right reserve TB Damai 2000 - {new Date().getFullYear()}            
+            All right reserve TB Damai 2001 - {new Date().getFullYear()}            
         </div>
     )
 }
