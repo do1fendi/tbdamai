6e679306f88f650b61ae1c2752e3e741ba5a87ac
Fendi <do1fendi@gmail.com>
2021-06-22 14:55:39 +0800

    added .nojekll to out root folder, then everything ok

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 module.exports = {
     basePath: '/tbdamai',
     assetPrefix: '/tbdamai/',
-    distDir: "_next",
     images: {
         loader: "imgix",
         path: "/tbdamai",
     },
     env: {
         BASEURL: 'http://localhost:3005/frontend',
-        BACKEND_URL: '/tbdamai',
     },
   }
