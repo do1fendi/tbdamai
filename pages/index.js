a417531510241b0c8395b0046a40c08659f43655
Fendi <do1fendi@gmail.com>
2021-06-22 16:46:35 +0800

    change react version to 16

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,20 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import MessengerCustomerChat from "react-messenger-customer-chat";
 
 export default function Home() {
   return (
-    <div className='container mx-auto px-4'>
+    <div className="container mx-auto px-4">
       <Head>
         <title>TBDamai | Home</title>
         <link rel="icon" href="/favicon.ico" />
-      </Head> 
+      </Head>
       <h1>Welcome to Tb Damai</h1>
+      <MessengerCustomerChat
+        pageId="100258215401523"
+        appId="329917598722443"
+        htmlRef="<REF_STRING>"
+      />
     </div>
-  )
+  );
 }
