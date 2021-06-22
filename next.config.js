1ebb61c895f8969ecb44528a5a19a69407d262a9
Fendi <do1fendi@gmail.com>
2021-06-22 09:25:49 +0800

    added basePath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     basePath: '/tbdamai',
-    assetPrefix: '/tbdamai/',
+    images: {
+        loader: "imgix",
+        path: "/tbdamai",
+    },
     env: {
         BASEURL: 'http://localhost:3005/frontend',
     },
