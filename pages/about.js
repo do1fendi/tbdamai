0201d2651143c0b25721b6712ec72be2880e1413
Fendi <do1fendi@gmail.com>
2021-07-15 18:10:17 +0800

    added about page

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,15 @@
 function about() {
     return (
         <>
-        <div className="flex flex-wrap flex-start justify-around">
+        <div className="text-center p-8 text-5xl text-gray-400">
+            About Us
+        </div>
+        <div className="flex flex-shrink justify-around">
+            <div className="rounded-full bg-blue-300 w-60 h-60 flex items-center justify-center"><p>1</p></div>
+            <div className="rounded-full bg-blue-300 w-60 h-60 flex items-center justify-center">2</div>
+            <div className="rounded-full bg-blue-300 w-60 h-60 flex items-center justify-center"><p>3</p></div>            
+        </div>
+        <div className="grid grid-cols-3 gap-4">
             <div className="rounded-full bg-blue-300 w-60 h-60 flex items-center justify-center"><p>1</p></div>
             <div className="rounded-full bg-blue-300 w-60 h-60 flex items-center justify-center">2</div>
             <div className="rounded-full bg-blue-300 w-60 h-60 flex items-center justify-center"><p>3</p></div>            
