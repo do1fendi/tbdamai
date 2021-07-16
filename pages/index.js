61d40f3e732723c4bba1fba1c0ab8fe5b06bd5af
unknown <do1fendi@gmail.com>
2021-07-16 22:32:53 +0800

    modify index

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 export default function Home() {
   const ctx = useContext(StoreContext)
-  
+
   // useEffect(() => {
   //   if (localStorage.getItem('token')) {
   //     (async () => {
@@ -33,9 +33,18 @@ export default function Home() {
         <title>TBDamai | Home</title>
         <link rel="icon" href="/tbdamai/favicon.ico" />
       </Head>
-      <div className="bg-blue-200 h-screen">Welcome to Tb Damai</div>
-      <div>
-        <Link href="/coran/tombak"><a><button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Tombak</button></a></Link>
+      <div className="bg-blue-200 h-48 w-full">Welcome to Tb Damai</div>
+      <div className="flex w-full">
+        <div className="w-1/3 h-48 flex justify-center items-center bg-gray-900">
+          <Link href="/coran/tombak"><a><button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Tombak</button></a></Link>
+        </div>
+        <div className="w-1/3 h-48 flex justify-center items-center bg-blue-200">
+          <Link href="/coran/tombak"><a><button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Tombak</button></a></Link>
+        </div>
+        <div className="w-1/3 h-48 flex justify-center items-center bg-blue-300">
+          <Link href="/coran/tombak"><a><button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Tombak</button></a></Link>
+        </div>
+
       </div>
     </div>
   );
